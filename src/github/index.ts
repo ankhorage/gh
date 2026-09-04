93f0bcb1ec3821ecbbb9cdc4121e426ba598f918
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-04 22:31:57 +0200

    feat: expose github repository adapter api

---
+++ src/github/index.ts
@@ -0,0 +1,8 @@
+export type { GitHubRepositoryConnectionOptions } from '../connection/definitions/GitHubRepositoryConnectionOptions.js';
+export type {
+  GitHubRepositoryConnectionFailure,
+  GitHubRepositoryConnectionResult,
+  GitHubRepositoryConnectionStatus,
+} from '../connection/definitions/GitHubRepositoryConnectionResult.js';
+export type { GitHubRepositoryVisibility } from '../connection/definitions/GitHubRepositoryVisibility.js';
+export { connectGitHubRepositoryAsync } from '../connection/services/connectGitHubRepositoryAsync.js';
